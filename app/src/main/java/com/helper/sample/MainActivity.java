b707ae34c335cb956807095aa11b404256ed0ce2
DroidHelios <droidhelios@gmail.com>
2020-10-22 14:25:21 +0530

    Update crashlytics version

--- app/src/main/java/com/helper/sample/MainActivity.java
+++ app/src/main/java/com/helper/sample/MainActivity.java
@@ -3,10 +3,14 @@ package com.helper.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.helper.task.TaskRunner;
 import com.helper.util.BaseUtil;
 import com.helper.util.StyleUtil;
+
+import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +22,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onOpenBrowser(View view) {
-        BaseUtil.openLinkInAppBrowser(this,"","");
+//        BaseUtil.openLinkInAppBrowser(this,"","");
+        TaskRunner.getInstance().executeAsync(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                for(int i= 0; i< 10; i++){
+                    Log.d("TaskRunner","Task1:" +i);
+                    Thread.sleep(100);
+                }
+                return null;
+            }
+        });
+        TaskRunner.getInstance().executeAsync(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                for(int i= 0; i< 10; i++){
+                    Log.d("TaskRunner","Task2:" +i);
+                    Thread.sleep(100);
+                }
+                return null;
+            }
+        });
     }
 
     @Override
--- helper-sdk/src/main/java/com/helper/util/StyleUtil.java
+++ helper-sdk/src/main/java/com/helper/util/StyleUtil.java
@@ -1,11 +1,13 @@
 package com.helper.util;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
-
+import android.view.WindowManager;
 
 
 public class StyleUtil {
@@ -24,6 +26,22 @@ public class StyleUtil {
 //                        controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
                         controller.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
                     }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setTranslucentStatus(Window window) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+    //            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                    window.setStatusBarColor(Color.parseColor("#FAFAFA"));
                 }
             }
         } catch (Exception e) {
