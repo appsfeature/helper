2f0bac4fb1f8cfa70e78b8ebb48377eb87828117
droidhelios <droidhelios@gmail.com>
2022-11-14 11:55:49 +0530

    Update constants

--- helper-sdk/src/main/java/com/helper/util/BaseConstants.java
+++ helper-sdk/src/main/java/com/helper/util/BaseConstants.java
@@ -12,16 +12,19 @@ public interface BaseConstants {
     String FAILURE = "failure";
     String NO_INTERNET_CONNECTION = "No Internet Connection";
     int ACTIVE = 1;
+    int DE_ACTIVE = 0;
     String WEB_VIEW_URL = "webViewUrl";
+    String ID = "id";
     String TITLE = "title";
-    String DEFAULT_DIRECTORY = "Helper";
     String CATEGORY_PROPERTY = "category_property";
     String EXTRA_PROPERTY = "extra_property";
+    String DEFAULT_DIRECTORY = "Helper";
 
     interface Error {
         String MSG_ERROR = "Error, please try later.";
         String DATA_NOT_FOUND = "Error, Data not found";
         String CATEGORY_NOT_FOUND = "Error, This is not supported. Please update";
+        String MSG_ERROR_INTEGRATION = "Error, Please contact to developer.";
     }
 
 }
