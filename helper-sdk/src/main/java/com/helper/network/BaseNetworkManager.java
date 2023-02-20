19b6d92867b729977899ca654746f83b698eb21b
droidhelios <droidhelios@gmail.com>
2023-02-20 15:34:21 +0530

    Update BaseNetworkManager

--- helper-sdk/src/main/java/com/helper/network/BaseNetworkManager.java
+++ helper-sdk/src/main/java/com/helper/network/BaseNetworkManager.java
@@ -17,14 +17,12 @@ public class BaseNetworkManager {
     public static String keyPdfPath = BaseConstants.DEFAULT_KEY_PDF_PATH;
 
     public interface ParserConfigData<T> {
-        void onSuccess(T t, String imagePath, String pdfPath);
-
+        default void onSuccess(T t) {}
+        default void onSuccess(T t, String imagePath, String pdfPath){}
         void onFailure(Exception error);
     }
-    public interface ParserConfigDataSimple<T> {
-        void onSuccess(T t);
 
-        void onFailure(Exception error);
+    public interface ParserConfigDataSimple<T> extends ParserConfigData<T>{
     }
 
     public static <T> List<T> addBaseUrlOnList(List<T> items, String imagePath, String pdfPath) {
@@ -45,22 +43,10 @@ public class BaseNetworkManager {
      * @param <T> : typecast
      */
     public static <T> void parseConfigData(String data, Type type, ParserConfigDataSimple<T> callback) {
-        try {
-            if (!BaseUtil.isEmptyOrNull(data)) {
-                final T t = GsonParser.getGson()
-                        .fromJson(data, type);
-                if (t != null) {
-                    callback.onSuccess(t);
-                } else {
-                    callback.onFailure(new Exception(BaseConstants.NO_DATA));
-                }
-            } else {
-                callback.onFailure(new Exception(BaseConstants.EMPTY_OR_NULL_DATA));
-            }
-        } catch (JsonSyntaxException e) {
-            e.printStackTrace();
-            callback.onFailure(e);
-        }
+         parseConfigData(data, null, type, callback);
+    }
+    public static <T> void parseConfigData(String data, Type type, ParserConfigData<T> callback) {
+         parseConfigData(data, null, type, callback);
     }
 
     /**
@@ -74,20 +60,22 @@ public class BaseNetworkManager {
         try {
             if (!BaseUtil.isEmptyOrNull(data)) {
                 JSONObject object = new JSONObject(data);
+                String finalData = data;
+                String imagePath = null, pdfPath = null;
                 if (!BaseUtil.isEmptyOrNull(object.optString(objectKey))) {
-                    String imagePath = object.optString(keyImagePath);
-                    String pdfPath = object.optString(keyPdfPath);
-                    final T t = GsonParser.getGson()
-                            .fromJson(object.optString(objectKey), type);
-                    if (t != null) {
-                        if (t instanceof BaseDataModel) {
-                            ((BaseDataModel) t).setImagePath(imagePath);
-                            ((BaseDataModel) t).setPdfPath(pdfPath);
-                        }
-                        callback.onSuccess(t, imagePath, pdfPath);
-                    } else {
-                        callback.onFailure(new Exception(BaseConstants.NO_DATA));
+                    imagePath = object.optString(keyImagePath);
+                    pdfPath = object.optString(keyPdfPath);
+                    finalData = object.optString(objectKey);
+                }
+                final T t = GsonParser.getGson()
+                        .fromJson(finalData, type);
+                if (t != null) {
+                    if (t instanceof BaseDataModel) {
+                        ((BaseDataModel) t).setImagePath(imagePath);
+                        ((BaseDataModel) t).setPdfPath(pdfPath);
                     }
+                    callback.onSuccess(t, imagePath, pdfPath);
+                    callback.onSuccess(t);
                 } else {
                     callback.onFailure(new Exception(BaseConstants.NO_DATA));
                 }
@@ -102,4 +90,23 @@ public class BaseNetworkManager {
             callback.onFailure(e);
         }
     }
+
+//    public static <T> void parseConfigData(String data, Type type, ParserConfigDataSimple<T> callback) {
+//        try {
+//            if (!BaseUtil.isEmptyOrNull(data)) {
+//                final T t = GsonParser.getGson()
+//                        .fromJson(data, type);
+//                if (t != null) {
+//                    callback.onSuccess(t);
+//                } else {
+//                    callback.onFailure(new Exception(BaseConstants.NO_DATA));
+//                }
+//            } else {
+//                callback.onFailure(new Exception(BaseConstants.EMPTY_OR_NULL_DATA));
+//            }
+//        } catch (JsonSyntaxException e) {
+//            e.printStackTrace();
+//            callback.onFailure(e);
+//        }
+//    }
 }
