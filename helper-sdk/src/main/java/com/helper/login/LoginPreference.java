faf42780797ec84c94d41ae42a16e6efb6a5f921
appsfeature <appsfeature@gmail.com>
2024-07-06 01:55:51 +0530

    Adding login2

--- helper-sdk/src/main/java/com/helper/login/LoginPreference.java
+++ helper-sdk/src/main/java/com/helper/login/LoginPreference.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 import com.google.gson.Gson;
 import com.helper.model.UserProfile;
 import com.helper.util.BasePrefUtil;
+import com.helper.util.GsonParser;
 
 public class LoginPreference extends BasePrefUtil {
     
@@ -14,14 +15,15 @@ public class LoginPreference extends BasePrefUtil {
     private static final String IS_REGISTRATION_COMPLETE = "isRegComplete";
     private static final String AUTHENTICATION_COMPLETE = "authenticationComplete";
 
-    private static final String LOGIN_JSON = "loginJson" ;
-    private static final String USER_NAME = "userName";
-    private static final String USER_PHOTO_URL = "photoUrl";
-    private static final String USER_ID_AUTO = "auto_id";
-    private static final String USER_EMAIL = "userEmail";
-    private static final String USER_MOBILE = "userMobile";
+    private static final String USER_ID = "loginUserId";
+    private static final String USER_NAME = "loginUserName";
+    private static final String USER_EMAIL = "loginUserEmail";
+    private static final String USER_MOBILE = "loginUserMobile";
+    private static final String USER_PHOTO_URL = "loginPhotoUrl";
     private static final String LOGIN_TYPE = "loginType";
     private static final String LOGIN_MODE = "loginMode";
+    private static final String LOGIN_EXTRAS = "loginExtras" ;
+    private static final String LOGIN_PROFILE = "loginProfile";
 
     /**
      * Set String value for a particular key.
@@ -30,35 +32,28 @@ public class LoginPreference extends BasePrefUtil {
         if (getDefaultSharedPref(context) != null && profile != null) {
             final SharedPreferences.Editor editor = getDefaultSharedPref(context).edit();
             if (editor != null) {
-                editor.putString(USER_ID_AUTO + loginType, getEmptyData(profile.getUserId()));
+                editor.putString(USER_ID + loginType, getEmptyData(profile.getUserId()));
                 editor.putString(USER_NAME + loginType, getEmptyData(profile.getUserName()));
                 editor.putString(USER_PHOTO_URL + loginType, getEmptyData(profile.getUserImage()));
                 editor.putString(USER_MOBILE + loginType, getEmptyData(profile.getMobileNo()));
                 editor.putString(USER_EMAIL + loginType, getEmptyData(profile.getEmailId()));
                 editor.putInt(LOGIN_TYPE + loginType, loginType);
                 editor.putInt(LOGIN_MODE + loginType, profile.getMode());
-                editor.putString(LOGIN_JSON + loginType, getEmptyData(profile.getExtras()));
+                editor.putString(LOGIN_EXTRAS + loginType, getEmptyData(profile.getExtras()));
+
+                editor.putString(LOGIN_PROFILE + loginType, profile.toJson());
                 editor.commit();
             }
         }
     }
 
+    public static String getProfileJson(Context context, int loginType) {
+        return getString(context, LOGIN_PROFILE + loginType);
+    }
+
     public static UserProfile getUserProfile(Context context, int loginType) {
-        UserProfile profile = new UserProfile();
-        if (getDefaultSharedPref(context) != null) {
-            final SharedPreferences editor = getDefaultSharedPref(context);
-            if (editor != null) {
-                profile.setUserId(editor.getString(USER_ID_AUTO + loginType, ""));
-                profile.setUserName(editor.getString(USER_NAME + loginType, ""));
-                profile.setMobileNo(editor.getString(USER_MOBILE + loginType, ""));
-                profile.setEmailId(editor.getString(USER_EMAIL + loginType, ""));
-                profile.setUserImage(editor.getString(USER_PHOTO_URL + loginType, ""));
-                profile.setType(editor.getInt(LOGIN_TYPE + loginType, 0));
-                profile.setMode(editor.getInt(LOGIN_MODE + loginType, 0));
-                profile.setExtras(editor.getString(LOGIN_JSON + loginType, ""));
-            }
-        }
-        return profile;
+        UserProfile userProfile = GsonParser.fromJsonAll(getProfileJson(context, loginType), UserProfile.class);
+        return (userProfile == null) ? new UserProfile() : userProfile;
     }
 
     public static String getUserName(Context context, int loginType) {
@@ -70,7 +65,7 @@ public class LoginPreference extends BasePrefUtil {
     }
 
     public static String getUserId(Context context, int loginType) {
-        return getString(context, USER_ID_AUTO + loginType);
+        return getString(context, USER_ID + loginType);
     }
 
     public static String getUserMobile(Context context, int loginType) {
@@ -81,12 +76,12 @@ public class LoginPreference extends BasePrefUtil {
         return getString(context, USER_EMAIL + loginType);
     }
 
-    public static String getProfileJson(Context context, int loginType) {
-        return getString(context, LOGIN_JSON + loginType);
+    public static String getExtrasJson(Context context, int loginType) {
+        return getString(context, LOGIN_EXTRAS + loginType);
     }
 
-    public static <T> T getProfileModel(Context context, int loginType, Class<T> classOfT) {
-        return new Gson().fromJson(getProfileJson(context, loginType), classOfT);
+    public static <T> T getExtrasModel(Context context, int loginType, Class<T> classOfT) {
+        return new Gson().fromJson(getExtrasJson(context, loginType), classOfT);
     }
 
     public static boolean isRegComplete(Context context, int loginType) {
